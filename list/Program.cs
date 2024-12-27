a35f1c05f53fda9c1958db3a5c139b1bfc543d3f
inciduran <inciduran010@gmail.com>
2024-12-27 07:42:35 +0300

    Proje dosyası ekle.

---
+++ list/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class okul
+    {
+        public string okuladı;
+        public List<string> öğrenci = new List<string>();
+        public void Oğrencıekle()
+        {
+            öğrenci.Add("0");
+
+        }
+        public void Öğrencilistele()
+        {
+            foreach (var item in öğrenci)
+            {
+                Console.WriteLine("item. ad+  item.soyad ");
+            }
+        }
+    }
+}
+          
+            
+        
+
+
